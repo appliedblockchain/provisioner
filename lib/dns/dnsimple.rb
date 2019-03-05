f693cc01f8befb6b10d4b7dcf187a00583c16db0
makevoid <makevoid@gmail.com>
2019-03-05 14:32:39 +0000

    add create-record sample call

--- lib/dns/dnsimple.rb
+++ lib/dns/dnsimple.rb
@@ -22,7 +22,8 @@ Minutes = -> { 60 }
 
 CACHE_TIME = 6 * Minutes.()
 
-CACHE_OFF = true
+# CACHE_OFF = true
+CACHE_OFF = false
 
 # R.flushdb
 
@@ -60,26 +61,39 @@ Records = -> (domain) {
   }
 }
 
-zones_list = Zones.()
-domains = zones_list.map &:name
-zones = {}
-domains.map do |domain|
-  zones[domain] = zones_list.find{ |zone| zone.name == domain }
-end
-
-for domain in domains
-  puts "Domain: #{domain}"
-  zone = zones.fetch domain
-  puts "  #{zone.inspect}\n"
-  records = Records.(domain)
-  for record in records
-    # puts "  #{record.inspect}\n"
-    name = record.name
-    name = "." if record.name == ""
-    type = record.type.ljust 3, ' '
-    puts "  #{type} - #{name} ⇒ #{record.content}\n"
+ListAll = -> {
+  zones_list = Zones.()
+  domains = zones_list.map &:name
+  zones = {}
+  domains.map do |domain|
+    zones[domain] = zones_list.find{ |zone| zone.name == domain }
   end
-  puts "\n"
-  exit
-end
-puts "---\n"
+
+  for domain in domains
+    puts "Domain: #{domain}"
+    zone = zones.fetch domain
+    puts "  #{zone.inspect}\n"
+    records = Records.(domain)
+    for record in records
+      # puts "  #{record.inspect}\n"
+      name = record.name
+      name = "." if record.name == ""
+      type = record.type.ljust 3, ' '
+      puts "  #{type} - #{name} ⇒ #{record.content}\n"
+    end
+    puts "\n"
+  end
+  puts "---\n"
+}
+
+CreateRecord = -> (domain, type, name, content) {
+  puts "Create Record: domain: #{domain.inspect}, type: #{type.inspect}, name: #{name.inspect}, content: #{content.inspect}"
+  resp = client.zones.create_zone_record(ACCOUNT_ID, domain, name: name, type: type, content: content)
+  puts resp.inspect
+  puts "created!"
+}
+
+ListAll.()
+
+domain, type, name, content = "abaub.io", "A", "test", "127.0.0.1"
+CreateRecord.(domain, type, name, content)
