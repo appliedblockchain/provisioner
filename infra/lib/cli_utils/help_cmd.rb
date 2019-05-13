cad40a40957d65cdf9895a79c3fcee88f14a2147
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2019-05-13 14:58:33 +0100

    update help so that rake commands are more user friendly

--- infra/lib/cli_utils/help_cmd.rb
+++ infra/lib/cli_utils/help_cmd.rb
@@ -6,32 +6,32 @@ module HelpCmd
   Commands:
 -------------
 
-  - CMD=provision rake - deploys a new infrastructure stack
-  - CMD=deploy rake - alias for provision
-  - CMD=destroy rake - destroys a stack (note: with great power comes great responsibility :D)
-  - CMD=list rake - lists the VMs and load balancers
-  - CMD=help rake
+  - rake CMD=provision  - deploys a new infrastructure stack
+  - rake CMD=deploy  - alias for provision
+  - rake CMD=destroy  - destroys a stack (note: with great power comes great responsibility :D)
+  - rake CMD=list  - lists the VMs and load balancers
+  - rake CMD=help
 
   Select the stack via STACK= (e.g. 'STACK=launchpad1' - 'STACK=launchpad1 CMD=list rake')
 
   Other examples:
 
     # change stack name
-    STACK_NAME=launchpad-ssr CMD=provision rake
+    rake STACK_NAME=launchpad-ssr CMD=provision
 
     # change VM size (2vcpu is the default one, big provisions a 4vcpu)
-    VM_SIZE=big CMD=provision rake
+    rake VM_SIZE=big CMD=provision
 
     # change the aws credentials profile
-    AWS_PROFILE_NAME=aws-credentials-name CMD=provision rake
+    rake AWS_PROFILE_NAME=aws-credentials-name CMD=provision
 
     # change the key pair name
-    KEY_PAIR_NAME=your-username CMD=provision rake
+    rake KEY_PAIR_NAME=your-username CMD=provision
 
     # usually you want to run deploy with these options command
-    STACK=stack-name AWS_PROFILE_NAME=aws-credentials-name KEY_PAIR_NAME=your-username CMD=provision rake
+    rake STACK=stack-name AWS_PROFILE_NAME=aws-credentials-name KEY_PAIR_NAME=your-username CMD=provision
 
-    # protip: create an env file to source with all your credentials and then just run `source env.sh && CMD=provision rake`
+    # protip: create an env file to source with all your credentials and then just run `source env.sh && rake CMD=provision`
   \n\n
 -------------
   ENV:
