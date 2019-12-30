2e0e10946f53eb3b21472188da5ac70fe69eaf47
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2019-12-30 10:21:00 +0000

    Update prereqs_lib.rb

--- lib/prereqs_lib.rb
+++ lib/prereqs_lib.rb
@@ -21,9 +21,7 @@ module PrereqsLib
 
   CopySSHPubKeys = -> {
     DEPLOYER_KEY = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDqP3bs2rrXNRZLdd19jXt/2i7gZA7BbkUUFhgBMca2baITdGQ2tzdbT2FOIBsAhfGVmNcITgeYa01YAOwpkbd/8bsbHGpn+0yHXsmeCDmBZQ6PG0xdy20U15uPn2a1QaMLtdkttfJquvhtwhd3HzVzLj3qZ+05T/GwRG0/TgJFoGgB9An/y0lWCMd9EfybPBhPeXXGEDaOvdniqWoqHdayE5+h8ar6+Phpei/DhKLKsHV3tTMTCLf1nbQZccbYNSFak4Yk8Ctd1mNSSUutdfVuEoUPH5MA9qe2CEQb6iPtAp/PnRaOCJrTReLPRdKjkPz6C7TPrpsZMEyiVzA+u1Nt deployer@appliedblockchain.com"
-    TIAGOACF_KEY = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDgGST8viwhPNfn5F/WjxUxzDRlYe6k+NwO7WXYULT3BRKd9rRNGEtvKs8evbXl1kxLdbBssw+Ppj6dBEWWZBlCSX0q/buqxL48hRDy+qttuC/DPqOYOPa0gb+d/wjA061jne4nyQQ5w+lWP+Iim6UdEItS7Pjk6jLTijPD7Rs9iLIXViRYRMfwV7MpcYItIGQAsZtceBjH26wlpttaKIdirC7JBp+BO7bCgPq1phIzH2+tmioRUfU/Orzmez6oUf4Wyw5+apC0a4GByGgA6AQORLstl+recuHRiTqdBUsEVnvZf5vfG8hYpbqUtcEnUP5ZwymRzKRof//Enu6JYH2x cardno:000606516550"
     ssh_all_exe_su "grep -qxF 'deployer@appliedblockchain.com' /root/.ssh/authorized_keys || echo \"#{DEPLOYER_KEY}\" >> /root/.ssh/authorized_keys"
-    ssh_all_exe_su "grep -qxF 'cardno:000606516550' /root/.ssh/authorized_keys || echo \"#{TIAGOACF_KEY}\" >> /root/.ssh/authorized_keys"
   }
 
   Prereqs = -> {
