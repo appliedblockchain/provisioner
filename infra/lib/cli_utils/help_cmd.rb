bdc8741a613d2b129369d2c405f56399114f23e5
makevoid <makevoid@gmail.com>
2020-05-06 23:59:42 +0100

    pretty print rake help

--- infra/lib/cli_utils/help_cmd.rb
+++ infra/lib/cli_utils/help_cmd.rb
@@ -32,22 +32,20 @@ module HelpCmd
     rake STACK=stack-name AWS_PROFILE_NAME=aws-credentials-name KEY_PAIR_NAME=your-username CMD=provision
 
     # protip: create an env file to source with all your credentials and then just run `source env.sh && rake CMD=provision`
-  \n\n
-
+  \n\n\n
    Scroll to the top for a list of commands
-
   \n\n
 -------------
   ENV:
 -------------
 
-Your environment variables:
+  Your environment variables:
 
-STACK_NAME: #{STACK_NAME}
-AWS_PROFILE_NAME: #{AWS_PROFILE_NAME}
-KEY_PAIR_NAME: #{KEY_PAIR_NAME}
-VM_SIZE: #{VM_SIZE}
-AWS_REGION: #{AWS_REGION}
+  STACK_NAME: #{STACK_NAME}
+  AWS_PROFILE_NAME: #{AWS_PROFILE_NAME}
+  KEY_PAIR_NAME: #{KEY_PAIR_NAME}
+  VM_SIZE: #{VM_SIZE}
+  AWS_REGION: #{AWS_REGION}
 \n\n"
   end
 
