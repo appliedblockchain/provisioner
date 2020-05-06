7ff49a2bf9df7e2a075ebcd3fd7a277069be49d0
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2020-05-06 23:58:51 +0100

    Update help_cmd.rb

--- infra/lib/cli_utils/help_cmd.rb
+++ infra/lib/cli_utils/help_cmd.rb
@@ -7,10 +7,10 @@ module HelpCmd
 -------------
 
   - rake CMD=provision  - deploys a new infrastructure stack
-  - rake CMD=deploy  - alias for provision
-  - rake CMD=destroy  - destroys a stack (note: with great power comes great responsibility :D)
-  - rake CMD=list  - lists the VMs and load balancers
-  - rake CMD=help
+  - rake CMD=deploy     - alias for provision
+  - rake CMD=destroy    - deletes a stack (note: with great power comes great responsibility :D)
+  - rake CMD=list       - lists the VMs and load balancers
+  - rake CMD=help       - displays this message
 
   Select the stack via STACK= (e.g. 'STACK=launchpad1' - 'STACK=launchpad1 CMD=list rake')
 
@@ -33,9 +33,15 @@ module HelpCmd
 
     # protip: create an env file to source with all your credentials and then just run `source env.sh && rake CMD=provision`
   \n\n
+
+   Scroll to the top for a list of commands
+
+  \n\n
 -------------
   ENV:
 -------------
+
+Your environment variables:
 
 STACK_NAME: #{STACK_NAME}
 AWS_PROFILE_NAME: #{AWS_PROFILE_NAME}
