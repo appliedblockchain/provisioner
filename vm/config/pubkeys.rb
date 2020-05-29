d7d898cb58319a44a7c3b8c4fef97c31935222b3
makevoid <makevoid@gmail.com>
2020-05-29 02:04:05 +0100

    git add ..

--- vm/config/pubkeys.rb
+++
@@ -1,7 +0,0 @@
-# add your ssh keys here
-DefinePubKeys = -> {
-  {
-    DEPLOYER_KEY: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDqP3bs2rrXNRZLdd19jXt/2i7gZA7BbkUUFhgBMca2baITdGQ2tzdbT2FOIBsAhfGVmNcITgeYa01YAOwpkbd/8bsbHGpn+0yHXsmeCDmBZQ6PG0xdy20U15uPn2a1QaMLtdkttfJquvhtwhd3HzVzLj3qZ+05T/GwRG0/TgJFoGgB9An/y0lWCMd9EfybPBhPeXXGEDaOvdniqWoqHdayE5+h8ar6+Phpei/DhKLKsHV3tTMTCLf1nbQZccbYNSFak4Yk8Ctd1mNSSUutdfVuEoUPH5MA9qe2CEQb6iPtAp/PnRaOCJrTReLPRdKjkPz6C7TPrpsZMEyiVzA+u1Nt deployer@appliedblockchain.com",
-    # YOUR_USERNAME: "ssh-...",
-  }
-}
