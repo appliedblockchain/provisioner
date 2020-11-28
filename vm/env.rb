3c589e53ada2bf4ec74c823c32f651957d083e1a
makevoid <makevoid@gmail.com>
2020-11-28 13:57:44 +0000

    fix when ip_b is blank - NOTE: cherry-pick to master

--- vm/env.rb
+++ vm/env.rb
@@ -2,11 +2,14 @@ require_relative 'lib/utils'
 require_relative 'lib/prereqs_lib'
 require_relative 'lib/setup-docker-swarm'
 
-USER = "admin"  # debian
+USER = "azureuser"  # azure
+# USER = "admin"  # debian
 # USER = "ubuntu" # ubuntu
 
 IP_A = ENV["IP_A"] || ENV["IP"]
-IP_B = ENV["IP_B"]
+ip_b = ENV["IP_B"]
+ip_b = nil if ip_b.empty?
+IP_B = ip_b
 
 raise "IP not found, please specify the two required IP_A, IP_B env vars or at least one (IP or IP_A)" unless IP_A
 
